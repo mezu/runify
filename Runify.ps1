4bf5a8522ef32514cebc09d788ae3a7da4e4761a
Jeanre Swanepoel <jeanre.swanepoel@onthedot.co.za>
2013-05-15 11:25:45 +0200

    initial commit

---
+++ Runify.ps1
@@ -0,0 +1,28 @@
+Stop-Process -processname nunit-agent
+$path =  "C:\Code\otd_cataloguing"
+$nunit = "$path\packages\NUnit.Runners.2.6.2\tools\nunit-color-console.exe"
+
+$configFile = "$path\runify.config"
+$config = New-Object XML
+$config.Load($configFile)
+
+$watcher = New-Object System.IO.FileSystemWatcher
+$watcher.Path = $path
+$watcher.IncludeSubdirectories = $true
+$watcher.EnableRaisingEvents = $true
+$watcher.Filter = "*Tests.dll"
+
+while($true){
+    
+    $change = $watcher.WaitForChanged([System.IO.WatcherChangeTypes]::Changed, 1000);
+    if($change.TimedOut){
+		continue;
+	}
+    
+	foreach($assembly in $config.tests.assembly) 
+	{
+		$assemblyPath = $assembly.path
+		$test = "$path\$assemblyPath"
+	    & $nunit /labels /nologo /framework:"net-4.0" /xml:"$OutDir\Tests.nunit.xml" $test
+	}
+}
